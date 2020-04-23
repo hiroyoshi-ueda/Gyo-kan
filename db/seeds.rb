1648466b789f13e3246a2800585960988fc69f21
hiroyoshi-ueda <torikara.h1139@gmail.com>
2020-04-23 04:50:50 +0000

    [fix]user_admin showページ

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,7 @@
 				   password_confirmation: "111111",
 				   image_id:1,
 				   image: File.open("./app/assets/images/temple1.jpeg")
-		          )
+		         )
 
 	Admin.create!( id: 2,
 				   name: "永平寺", name_kana: "エイヘイジ",
@@ -27,7 +27,45 @@
 				   tel: "0776633102",
 				   password: "222222",
 				   password_confirmation: "222222",
-		          )
+		         )
 
+	User.create!(
+				  fam_name: "佐藤", fam_name_kana:"サトウ",
+				  first_name: "太郎", first_name_kana: "タロウ",
+				  post_code: "1234",
+				  address: "東京都渋谷区神南",
+				  domicle: "青森県八戸市",
+				  email: "q@q",
+				  tel: "09011111111",
+				  admin_id: 1,
+				  password:"111111",
+				  password_confirmation: "111111"
+				)
+
+	User.create!(
+				  fam_name: "山田", fam_name_kana:"ヤマダ",
+				  first_name: "次郎", first_name_kana: "ジロウ",
+				  post_code: "5678",
+				  address: "東京都中央区日本橋",
+				  domicle: "青森県八戸市",
+				  email: "z@z",
+				  tel: "09022222222",
+				  admin_id: 2,
+				  password:"222222",
+				  password_confirmation: "222222"
+				)
+
+	User.create!(
+				  fam_name: "田中", fam_name_kana:"タナカ",
+				  first_name: "三郎", first_name_kana: "サブロウ",
+				  post_code: "4321",
+				  address: "東京都江戸川区瑞江",
+				  domicle: "青森県八戸市",
+				  email: "w@w",
+				  tel: "09033333333",
+				  admin_id: 1,
+				  password:"333333",
+				  password_confirmation: "333333"
+				)
 
 
